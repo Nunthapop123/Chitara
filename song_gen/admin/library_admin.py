72d05c42fcacd292acf697281caae8d4273d60a6
Nunthapop Nganiam <nuntha_pop@hotmail.com>
2026-04-25 11:25:43 +0700

    feat: make admin able to see RegisteredUser library

--- song_gen/admin/library_admin.py
+++ song_gen/admin/library_admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from song_gen.models import Library
+from song_gen.models import Library, GeneratedSong
+
+
+class GeneratedSongInline(admin.TabularInline):
+    model = GeneratedSong
+    extra = 0
+    readonly_fields = ('title', 'song_genre', 'status', 'created_at')
+    can_delete = False
 
 
 @admin.register(Library)
 class LibraryAdmin(admin.ModelAdmin):
     list_display = ('owner',)
     search_fields = ('owner__email',)
+    inlines = [GeneratedSongInline]
